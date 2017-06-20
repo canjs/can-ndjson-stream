ddc88170bfcf7f631a667e6d99ba926a02218476
bgando <gandolfo.bianca@gmail.com>
2017-06-20 10:02:00 -0500

    Remove es6 and update demo

--- demo/server.js
+++ demo/server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var http = require('http');
 var fs = require('fs');
 var ndjson = require('ndjson');
 
--- test-sauce-labs.js
+++ test-sauce-labs.js
@@ -1,4 +1,3 @@
-/* jshint esversion: 6 */
 'use strict';
 
 var testSauceLabs = require('test-saucelabs');
