304fcd5f59e143b4bfd3d0102a7dbc80af99c87a
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-23 21:11:40 +0100

    Revert to previous saucelabs config

--- test-sauce-labs.js
+++ test-sauce-labs.js
@@ -2,42 +2,25 @@
 
 var testSauceLabs = require('test-saucelabs');
 
-var maxDuration = 1800; // seconds, default 1800, max 10800
-var commandTimeout = 600; // seconds, default 300, max 600
-var idleTimeout = 1000; // seconds, default 90, max 1000
-
 // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
 var platforms = [{
-  browserName: 'firefox',
+  browserName: 'chrome',
+  platform: 'OS X 10.11',
+  version: '58.0'
+},{
+  browserName: 'chrome',
+  platform: 'OS X 10.9',
+  version: '54.0'
+},
+{
+  browserName: 'chrome',
   platform: 'Windows 10',
-  version: 'latest',
-  maxDuration: maxDuration,
-  commandTimeout: commandTimeout,
-  idleTimeout: idleTimeout
-}, {
-  browserName: 'googlechrome',
-  platform: 'OS X 10.12',
-  version: 'latest',
-  maxDuration: maxDuration,
-  commandTimeout: commandTimeout,
-  idleTimeout: idleTimeout
-}, {
-  browserName: 'safari',
-  platform: 'OS X 10.13',
-  version: '11',
-  maxDuration: maxDuration,
-  commandTimeout: commandTimeout,
-  idleTimeout: idleTimeout
-}, {/*
-  browserName: 'internet explorer',
-  platform: 'Windows 10',
-  version: '11.0'
-}, { */
-  browserName: 'MicrosoftEdge',
-  platform: 'Windows 10',
-  maxDuration: maxDuration,
-  commandTimeout: commandTimeout,
-  idleTimeout: idleTimeout
+  version: '59.0'
+},
+{
+  browserName: 'chrome',
+  platform: 'Windows 7',
+  version: '73.0'
 }];
 
 var url = 'http://localhost:3000/test.html';
